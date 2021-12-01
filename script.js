eaad130a3d65ab3db81f96bedaac1a10e777279a
shubhamNivilkar <nivshubham555@gmail.com>
2021-12-01 22:45:41 +0530

    Add API

---
+++ script.js
@@ -0,0 +1,28 @@
+const userInput = document.querySelector("#user-input");
+const translateBtn = document.querySelector("#translate-btn");
+const textOutput = document.querySelector("#text-output");
+
+
+//API url
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+/*translateBtn.addEventListener("click", translateText);
+
+function translateText(){
+    var text1 = userInput.value;
+    var newUrl = `${url}?text=${text1}`;
+
+    fetch(newUrl)
+        .then(response => response.json())
+        .then(response => {
+            var translatedText = response.contents.translated;
+            console.log(translateText)
+            textOutput.innerText = translatedText;
+        }).catch(err => {
+            console.log("Server is down try after sometime")
+            alert("Server error Try again later")
+        } )
+
+} 
+*/
